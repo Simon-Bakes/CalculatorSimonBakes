d4f881944629f5ceacbbfbc9f23fc6ae32a62449
Simon-Bakes <155891368+Simon-Bakes@users.noreply.github.com>
2024-01-17 15:29:17 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,21 +1,10 @@
-/*let x;
-let y;
-
-function part1(){
-    x = document.getElementById("output").value;
-    document.getElementById("output").value = "";
-}
-function part2(){
-    let z = x*y;
-    document.getElementById("output").value = +z;
-}
-function equalsCode(){
-    y = document.getElementById("output").value;
-    part2();
-}
 function clearCode(){
     document.getElementById("output").value = "";
 }
+function backspace(){
+    let currentOutput = document.getElementById("output").value;
+    document.getElementById("output").value = currentOutput.slice(0, -1);
+}
 function numCode(buttonValue) {
     document.getElementById("output").value += buttonValue;
-}*/
+}
