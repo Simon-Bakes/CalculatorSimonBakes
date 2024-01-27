20eab78fa24e8b18d734c6d883c2296765554b45
Simon-Bakes <155891368+Simon-Bakes@users.noreply.github.com>
2024-01-27 14:11:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -20,11 +20,54 @@ function plusminusOutputNum(){
     document.getElementById("outputNum").value = -numericValue;
 }
 
-function equalsOutputNum(){
-    try{
-    document.getElementById("outputNum").value = eval(document.getElementById("outputNum").value);
+let x;
+let y;
+let symbol;
+let z;
+let decide = 0;
+
+function equalsOutputNum(){ 
+    y = document.getElementById("outputNum").value;
+    if(decide==1){
+    z = Math.round(x/y*100)/100;}
+    else if(decide==2){
+        z = x*y;
+    }else if(decide==3){
+        z = x-y;
+    }else{
+        z = +x+ +y;
     }
-    catch(error){
-        document.getElementById("outputNum").value = "Error";
-    }
+    document.getElementById("outputNum").value = z;
+    document.getElementById("outputHistory").value += y+"=";
+}
+
+function div(){
+    decide += 1;
+    x = document.getElementById("outputNum").value.slice(0, -1);
+    symbol = document.getElementById("outputNum").value;
+    document.getElementById("outputHistory").value = x+symbol.slice(-1);
+    document.getElementById("outputNum").value = "";
+}
+
+function multi(){
+    decide += 2;
+    x = document.getElementById("outputNum").value.slice(0, -1);
+    symbol = document.getElementById("outputNum").value;
+    document.getElementById("outputHistory").value = x+symbol.slice(-1);
+    document.getElementById("outputNum").value = "";
+}
+
+function sub(){
+    decide += 3;
+    x = document.getElementById("outputNum").value.slice(0, -1);
+    symbol = document.getElementById("outputNum").value;
+    document.getElementById("outputHistory").value = x+symbol.slice(-1);
+    document.getElementById("outputNum").value = "";
+}
+
+function add(){
+    x = document.getElementById("outputNum").value.slice(0, -1);
+    symbol = document.getElementById("outputNum").value;
+    document.getElementById("outputHistory").value = x+symbol.slice(-1);
+    document.getElementById("outputNum").value = "";
 }
