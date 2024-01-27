46bb55c4249981e9d4ba6392102e84a457febfd4
Simon-Bakes <155891368+Simon-Bakes@users.noreply.github.com>
2024-01-27 17:23:00 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let x;
+let y;
+let symbol;
+let z;
+let decide = 0;
+
+/*Limits output display to 10*/
 function updateOutputNum(value){
     let outputNumInput = document.getElementById("outputNum");
     if(outputNumInput.value.length <= 9){
@@ -5,27 +12,36 @@ function updateOutputNum(value){
     }
 }
 
+/*Clears both displays*/
 function clearOutputNum(){
     document.getElementById("outputNum").value = "";
+    document.getElementById("outputHistory").value = "";
 }
 
+/*Clears entry*/
+function clearEntryOutputNum(){
+    if(decide==0){
+    document.getElementById("outputNum").value = "";
+    document.getElementById("outputHistory").value = "";}
+    else{
+        document.getElementById("outputNum").value = "";
+    }
+}
+
+/*Backspace function*/
 function backspaceOutputNum(){
     let currentOutput = document.getElementById("outputNum").value;
     document.getElementById("outputNum").value = currentOutput.slice(0, -1);
 }
 
+/*Reverses the signs*/
 function plusminusOutputNum(){
     let currentOutput = document.getElementById("outputNum").value;
     let numericValue = +currentOutput;
     document.getElementById("outputNum").value = -numericValue;
 }
 
-let x;
-let y;
-let symbol;
-let z;
-let decide = 0;
-
+/*Checks which math symbol and calculates*/
 function equalsOutputNum(){ 
     y = document.getElementById("outputNum").value;
     if(decide==1){
@@ -34,38 +50,60 @@ function equalsOutputNum(){
         z = x*y;
     }else if(decide==3){
         z = x-y;
-    }else{
+    }else if(decide==4){
         z = +x+ +y;
+    }else{
+        z = document.getElementById("outputNum").value;
     }
     document.getElementById("outputNum").value = z;
     document.getElementById("outputHistory").value += y+"=";
+    decide = 0;
 }
 
 function div(){
     decide += 1;
-    x = document.getElementById("outputNum").value.slice(0, -1);
-    symbol = document.getElementById("outputNum").value;
-    document.getElementById("outputHistory").value = x+symbol.slice(-1);
-    document.getElementById("outputNum").value = "";
+    getX();
 }
 
 function multi(){
     decide += 2;
-    x = document.getElementById("outputNum").value.slice(0, -1);
-    symbol = document.getElementById("outputNum").value;
-    document.getElementById("outputHistory").value = x+symbol.slice(-1);
-    document.getElementById("outputNum").value = "";
+    getX();
 }
 
 function sub(){
     decide += 3;
-    x = document.getElementById("outputNum").value.slice(0, -1);
-    symbol = document.getElementById("outputNum").value;
-    document.getElementById("outputHistory").value = x+symbol.slice(-1);
-    document.getElementById("outputNum").value = "";
+    getX();
 }
 
 function add(){
+    decide += 4;
+    getX();
+}
+
+function power(){
+    x = document.getElementById("outputNum").value;
+    z = x*x;
+    document.getElementById("outputNum").value = z;
+    document.getElementById("outputHistory").value = "sqr("+x+")";
+}
+
+function sqrtrt(){
+    x = document.getElementById("outputNum").value;
+    z = Math.sqrt(x);
+    z = z.toFixed(4);
+    document.getElementById("outputNum").value = z;
+    document.getElementById("outputHistory").value = "√("+x+")";
+}
+
+function overX(){
+    x = document.getElementById("outputNum").value;
+    z = 1/x;
+    document.getElementById("outputNum").value = z;
+    document.getElementById("outputHistory").value = "1/("+x+")";
+}
+
+/*Changes history input*/
+function getX(){
     x = document.getElementById("outputNum").value.slice(0, -1);
     symbol = document.getElementById("outputNum").value;
     document.getElementById("outputHistory").value = x+symbol.slice(-1);
